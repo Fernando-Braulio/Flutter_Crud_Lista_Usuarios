11920db917f0beaa75df3832b9c9238503c95173
Fernando-Braulio <fernandobraulio@outlook.com.br>
2021-02-21 15:05:24 -0300

    displaying the list of users

---
+++ lib/Views/User_List.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_lista_usuario/Components/User_Tile.dart';
+import 'package:flutter_crud_lista_usuario/Data/Dummy_Users.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+      ),
+    );
+  }
+}
