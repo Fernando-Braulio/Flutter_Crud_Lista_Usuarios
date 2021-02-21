4214b621ccf20fa9d660c8f1f1c83ff2168f63f7
Fernando-Braulio <fernandobraulio@outlook.com.br>
2021-02-21 14:40:21 -0300

    Insert model and dummy users

---
+++ lib/Data/Dummy_Users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud_lista_usuario/Models/User.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@aa.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_1280.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Joana',
+    email: 'ghjkkjhgfd@aa.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/12/21/00/36/woman-1921883_1280.jpg',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Julia',
+    email: 'dfghfgh@aa.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/18/09/12/woman-163426__480.jpg',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Amanda',
+    email: 'sdfasdfg@aa.com',
+    avatarUrl: '',
+  ),
+};
